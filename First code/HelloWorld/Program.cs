ffb1ebcbd9a39253b2f69f10cd7b424c4657e174
Your Name <you@example.com>
2021-05-03 16:59:20 -0500

    added Hello World

---
+++ First code/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //This is the starting point of application
+    class Program
+    {
+        static void Main(string[] args) //Main method
+        {
+            //TO run your console app, run dotnet run on the folder that contains the .NET project
+            // dotnet build -> makes sure that your code compiles
+            // dotnet run -> builds and runs your code
+           Console.WriteLine("Hello World!");
+        }
+    }
+}
